6d2f221da44044e7f88c2b539f569f8ea6fb1f6f
Student <Student@zpr-l0007075.local>
2017-04-06 08:35:10 -0400

    Initial Commit

---
+++ Collaborative Reading App 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Collaborative Reading App 2
+//
+//  Created by Student on 2017-04-06.
+//  Copyright © 2017 Daniel Dobre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
